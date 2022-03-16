b3ad891c3ddb9d5f232e36ed177bc0baab400f48
David29-ra <davidrivasa.12@gmail.com>
2022-03-15 23:45:47 -0500

    Add some changes

--- scripts/pages/loadingPage.js
+++ scripts/pages/loadingPage.js
@@ -3,7 +3,7 @@ export const loadingPage = (() => {
     render: () => {
       return `
         <div class="loading">
-          <img src="https://img.pikbest.com/58pic/35/39/61/62K58PICb88i68HEwVnm5_PIC2018.gif!w340" alt="gift_loading"/>
+          <img src="./assets/gifts/Hourglass.gif" alt="gift_loading"/>
         </div>
       `;
     },
